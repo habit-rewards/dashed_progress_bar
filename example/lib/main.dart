3c28c5e93c7d73e2c0daf9b0525086cab96ca695
Habit Rewards <hi.habitrewards@gmail.com>
2025-01-18 12:37:14 +0800

    setup examples

---
+++ example/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:dashed_circular_progress_bar/dashed_circular_progress_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashed Circular Progress Bar Demo'),
+        ),
+        body: Center(
+          child: Container(
+            width: 18,
+            height: 18,
+            color: const Color.fromARGB(255, 255, 255, 255), 
+            child: DashedCircularProgressBar.square(
+              dimensions: 100,
+              // startAngle: 25,
+              progress: 4,
+              maxProgress: 10,
+              foregroundColor: Color.fromARGB(255, 20, 123, 37),
+              backgroundColor: Color.fromARGB(255, 138, 138, 138),
+              foregroundStrokeWidth: 2,
+              backgroundStrokeWidth: 2,
+              foregroundGapSize: 30,
+              backgroundGapSize: 30,
+              // foregroundDashSize: 100,
+              // backgroundDashSize: 100,
+              animation: true,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
